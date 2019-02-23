af98397dd1e2c41dac9d68534b5a15f9481f72db
Raylson Alves <raaylsonalves@gmail.com>
2019-02-22 23:39:32 -0300

    Branch inicial, caminho do xml com a pasta do usuario, removido o metodo listar do AuthController

--- src/Identity/Identity.API/Controllers/AuthController.cs
+++ src/Identity/Identity.API/Controllers/AuthController.cs
@@ -10,12 +10,6 @@ namespace Identity.Api.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
         // GET api/values/5
         [HttpGet("{id}")]
